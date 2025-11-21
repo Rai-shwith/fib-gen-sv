2d6cdd4412c9d3672b3f0386cb45ca6e078aa568
Ashwith Rai <147788165+Rai-shwith@users.noreply.github.com>
2025-11-21 09:41:08 +0530

    Implement full adder module in Verilog

---
+++ design/fulladder.v
@@ -0,0 +1,7 @@
+// Code your design here
+module fa(in1,in2,cin,sum,cout);
+  input in1,in2,cin;
+  output sum,cout;
+  assign sum = in1^in2^cin;
+  assign cout = (in1&in2) | (in2&cin) | (cin&in1)
+endmodule
